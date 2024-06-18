7ff80d70f3e5b79260cababc4b10523a3ed63e0f
Dante Puddu <dantepuddu@hotmail.com>
2024-06-18 03:06:59 -0300

    Adding start instructions to README.

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-const twitchUserName = "your-twitch-username"; // Nombre de usuario de Twitch
+const twitchUserName = "your-twitch-username"; //  <-- CHANGE / Twitch Username
 const twitchClient = new tmi.Client({
   options: { debug: true },
   connection: {
@@ -20,9 +20,9 @@ const twitchClient = new tmi.Client({
   },
   identity: {
     username: twitchUserName,
-    password: 'oauth:your-auth-token' // Token de autenticación de Twitch
+    password: 'oauth:your-auth-token' //  <-- CHANGE / Twitch Token Authentication
   },
-  channels: ['channel-to-connect'] // Canal al que se conectará el bot
+  channels: ['twitch-channel-to-connect'] //  <-- CHANGE / Twitch Channel that the bot will connect
 });
 
 twitchClient.connect();
@@ -31,20 +31,19 @@ let answersCount = {};
 let activeQuestionIndex = -1;
 let timerInterval = null; // Temporizador para el contador de 15 segundos
 
-// Función para iniciar el temporizador de 15 segundos
+// Function to start a 15 seconds timer.
 const startCountdownTimer = () => {
   let countdown = 0;
   timerInterval = setInterval(() => {
     countdown++;
 
-    // Cuando han pasado 15 segundos (15000 milisegundos), enviamos el mensaje al chat de Twitch
     if (countdown === 15) {
-      twitchClient.say(twitchUserName, '15 seconds remaining!!' );
+      twitchClient.say(twitchUserName, '15 seconds remaining!!' ); // Send message to twitch chat.
     }
-  }, 1000); // Intervalo de 1 segundo
+  }, 1000); 
 };
 
-// Escuchar mensajes del chat de Twitch
+// Function to keep twitch chat messages.
 twitchClient.on('message', (channel, tags, message, self) => {
   if (self || activeQuestionIndex === -1) return;
   const word = message.trim().toLowerCase();
@@ -64,18 +63,18 @@ io.on('connection', (socket) => {
     activeQuestionIndex = questionIndex;
     console.log('Game started to Question number:', questionIndex + 1);
 
-    // Iniciar el temporizador de 15 segundos
+    // Start 15 seconds timer.
     startCountdownTimer();
 
-    // Enviamos el mensaje al chat de Twitch
+    // Send message to twitch chat.
     twitchClient.say(twitchUserName, 'Gathering responses! You have 30 seconds to type your answer in the chat, and the 6 most repeated ones will be assigned!');
   });
 
   socket.on('endGame', ({ questionIndex }) => {
     if (questionIndex === activeQuestionIndex) {
-      clearInterval(timerInterval); // Limpiar el temporizador si el juego ha terminado
+      clearInterval(timerInterval);
 
-      // Ordenar las respuestas por cantidad y seleccionar las 6 más repetidas
+      // Sort the answers by quantity and select the 6 most repeated.
       const sortedAnswers = Object.keys(answersCount)
         .map(word => ({ word, count: answersCount[word] }))
         .sort((a, b) => b.count - a.count)
@@ -88,12 +87,11 @@ io.on('connection', (socket) => {
           };
         });
 
-      // Emitir las respuestas ordenadas al cliente
       io.emit('showAnswers', { answers: sortedAnswers, questionIndex });
       activeQuestionIndex = -1;
       console.log('Game ended to Question number:', questionIndex + 1, sortedAnswers);
 
-      // Enviamos el mensaje al chat de Twitch
+      // Send message to twitch chat.
       twitchClient.say(twitchUserName, 'Answers asigned. Stop spamming!!');
     }
   });
